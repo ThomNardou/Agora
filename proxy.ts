c1fc545c4374255ea940f639c447f370d8f8eaca
Tytan <thomasnardou@icloud.com>
2026-05-20 15:44:38 +0200

    chore(env): change variable name

--- proxy.ts
+++ proxy.ts
@@ -10,7 +10,7 @@ export async function proxy(request: NextRequest) {
     }
 
     const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/login`,
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/admin/login`,
         {
             headers: { Cookie: request.headers.get("cookie") || "" },
         },
